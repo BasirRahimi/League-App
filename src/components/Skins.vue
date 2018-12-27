404fdcdd9acb93c1bf20faad4f2c9e4e543f72fa
Basir <basiralirahimi@gmail.com>
2018-12-27 23:20:51 +0000

    skins component loading images added

--- src/components/Skins.vue
+++ src/components/Skins.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-carousel class="carousel" height="717">
-    <v-carousel-item v-for="skin in skins" :key="skin.id" :src="'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+championId+'_'+skin.num+'.jpg'">
+  <v-carousel class="carousel" :class="{loading: loading}" :height="height" :hide-controls="loading" :hide-delimiters="loading">
+    <v-carousel-item v-for="skin in skins" :key="skin.id" :src="imgSrc(skin)">
     </v-carousel-item>
   </v-carousel>
 </template>
@@ -13,15 +13,37 @@ export default {
     },
     championId: {
       type: String
+    },
+    loading: {
+      type: Boolean
     }
   },
   data() {
     return {
-
+      height: '717'
+    }
+  },
+  beforeMount() {
+    if(this.loading) {
+      this.height = '560'
+    }
+  },
+  methods: {
+    imgSrc(skin) {
+      if(this.loading) {
+        return 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/'+this.championId+'_'+skin.num+'.jpg'
+      } else {
+        return 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+this.championId+'_'+skin.num+'.jpg'
+      }
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-
+.carousel {
+  max-width: 1217px;
+  &.loading {
+    max-width: 308px;
+  }
+}
 </style>
--- src/views/Champion.vue
+++ src/views/Champion.vue
@@ -1,10 +1,7 @@
 <template>
   <v-container fluid>
-    <!-- <v-avatar size="240">
-      <img :src="'http://ddragon.leagueoflegends.com/cdn/'+$store.state.lolVersion+'/img/champion/'+champion.image.full" :alt="champion.name">
-    </v-avatar> -->
-    <h1>Skins</h1>
-    <skins :skins="champion.skins" :champion-id="$route.params.id"/>
+    
+    <skins :skins="champion.skins" :champion-id="$route.params.id" loading/>
   </v-container>
 </template>
 
@@ -23,13 +20,10 @@ export default {
   beforeMount() {
     this.$http.get('http://ddragon.leagueoflegends.com/cdn/'+this.$store.state.lolVersion+'/data/en_US/champion/'+this.$route.params.id+'.json')
     .then(response=>{
-      this.champion = response.body.data[this.$route.params.id];
+      this.champion = response.body.data[this.$route.params.id]
     })
   }
 }
 </script>
 <style lang="scss" scoped>
-.carousel {
-  max-width: 1217px;
-}
 </style>
